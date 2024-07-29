481d1550de96a58728c41cdd13eef4c917c0824e
6urk <cabudi10@gmail.com>
2024-07-28 22:23:57 -0500

    admin page

---
+++ rideSwap/src/main/java/com/_urk/rideSwap/RideSwapApplication.java
@@ -0,0 +1,13 @@
+package com._urk.rideSwap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideSwapApplication.class, args);
+	}
+
+}
---
+++ rideSwap/src/test/java/com/_urk/rideSwap/RideSwapApplicationTests.java
@@ -0,0 +1,13 @@
+package com._urk.rideSwap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideSwapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
